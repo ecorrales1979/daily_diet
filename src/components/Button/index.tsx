2213a0473e52aefc5a9bb81e6eabb1ed799f8f02
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-06-25 19:33:07 -0300

    feat: create a button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,20 @@
+import { type Feather } from '@expo/vector-icons'
+import { TouchableOpacityProps } from 'react-native';
+
+import { Container, Icon, Label, type ButtonProps } from './styles';
+
+interface Props extends TouchableOpacityProps, ButtonProps {
+  label: string;
+  icon?: keyof typeof Feather.glyphMap
+}
+
+export function Button({ icon, label, variant, ...rest }: Props) {
+  return (
+    <Container variant={variant} {...rest}>
+      {icon && (
+        <Icon name={icon} variant={variant} />
+      )}
+      <Label variant={variant}>{label}</Label>
+    </Container>
+  );
+}
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,46 @@
+import styled, { css } from 'styled-components/native';
+import { Feather } from '@expo/vector-icons';
+
+export type ButtonStyleType = 'contained' | 'outlined';
+
+export interface ButtonProps {
+  variant: ButtonStyleType;
+  fullWidth?: boolean;
+}
+
+export const Container = styled.TouchableOpacity<ButtonProps>`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 6px;
+  ${({ fullWidth, theme, variant }) => css`
+    background-color: ${variant === 'contained'
+      ? theme.palette.gray_2
+      : 'transparent'};
+    border: ${variant === 'contained'
+      ? theme.palette.gray_2
+      : theme.palette.gray_1};
+    margin: 0 ${fullWidth ? 0 : 'auto'};
+  `}
+  border-radius: 6px;
+  padding: 16px 24px;
+`;
+
+export const Label = styled.Text<ButtonProps>`
+  ${({ theme, variant }) => css`
+    color: ${variant === 'contained'
+      ? theme.palette.white
+      : theme.palette.gray_1};
+    font-size: ${theme.fontSize.sm}px;
+    font-family: ${theme.fontFamily.bold};
+  `};
+  color: ${({ theme, variant }) =>
+    variant === 'contained' ? theme.palette.white : theme.palette.gray_1};
+`;
+
+export const Icon = styled(Feather).attrs<ButtonProps>(
+  ({ theme, variant }) => ({
+    size: 18,
+    color: variant === 'contained' ? theme.palette.white : theme.palette.gray_1,
+  })
+)``;
