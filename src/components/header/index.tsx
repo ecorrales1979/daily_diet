3bcc59a3b27e6ea6b9fa2eb8fc185e99c026e2ab
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-06-26 00:21:12 -0300

    feat: create header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,12 @@
+import { Avatar, Container, Logo } from './styles';
+
+import LogoImg from '../../assets/logo.png'
+
+export function Header() {
+  return (
+    <Container>
+      <Logo source={LogoImg} />
+      <Avatar />
+    </Container>
+  )
+}
---
+++ src/components/header/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 24px;
+`;
+
+export const Logo = styled.Image`
+  width: 82px;
+  height: 37px;
+`;
+
+export const Avatar = styled.View`
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  border: 2px solid #000;
+  background-color: ${({ theme }) => theme.palette.gray_5};
+`;
