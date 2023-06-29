3ab663a8c9d46cc9e0a02289523a575672b72eab
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-06-28 23:49:56 -0300

    style: adjust gap on button content

--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -1,18 +1,18 @@
-import styled, { css } from 'styled-components/native';
-import { Feather } from '@expo/vector-icons';
+import styled, { css } from 'styled-components/native'
+import { Feather } from '@expo/vector-icons'
 
-export type ButtonStyleType = 'contained' | 'outlined';
+export type ButtonStyleType = 'contained' | 'outlined'
 
 export interface ButtonProps {
-  variant: ButtonStyleType;
-  fullWidth?: boolean;
+  variant: ButtonStyleType
+  fullWidth?: boolean
 }
 
 export const Container = styled.TouchableOpacity<ButtonProps>`
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  gap: 6px;
+  gap: 12px;
   ${({ fullWidth, theme, variant }) => css`
     background-color: ${variant === 'contained'
       ? theme.palette.gray_2
@@ -24,7 +24,7 @@ export const Container = styled.TouchableOpacity<ButtonProps>`
   `}
   border-radius: 6px;
   padding: 16px 24px;
-`;
+`
 
 export const Label = styled.Text<ButtonProps>`
   ${({ theme, variant }) => css`
@@ -36,11 +36,11 @@ export const Label = styled.Text<ButtonProps>`
   `};
   color: ${({ theme, variant }) =>
     variant === 'contained' ? theme.palette.white : theme.palette.gray_1};
-`;
+`
 
 export const Icon = styled(Feather).attrs<ButtonProps>(
   ({ theme, variant }) => ({
     size: 18,
-    color: variant === 'contained' ? theme.palette.white : theme.palette.gray_1,
+    color: variant === 'contained' ? theme.palette.white : theme.palette.gray_1
   })
-)``;
+)``
